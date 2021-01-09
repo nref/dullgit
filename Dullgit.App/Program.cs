285c5ed82c97b59a1a92a40671f91a486aa12ab5
Doug Slater <doug@sltr.us>
2021-01-09 06:57:18 -0500

    Add Solution and first projects

---
+++ Dullgit.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dullgit.App
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("dullgit: A dull implementation of Git in C#");
+    }
+  }
+}
